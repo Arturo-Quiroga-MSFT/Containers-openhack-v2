a34e23d7ba781839b7fb8e6d9174de3a52460a30
Arturo Quiroga <arturoqu@microsoft.com>
2021-07-29 13:49:59 -0400

    added info to dockerfiles

---
+++ byos-output-jul-29-2021.sh
@@ -0,0 +1,13 @@
+Creating Azure SQL DB...
+Name         Tier            Family    Capacity    MaxSize    ElasticPool
+-----------  --------------  --------  ----------  ---------  -------------
+mydrivingDB  GeneralPurpose  Gen5      2           32GB
+
+SQL database created successfully.
+Name      ResourceGroup     Status    Image                  CPU/Memory       OsType    Location
+--------  ----------------  --------  ---------------------  ---------------  --------  -------------
+dataload  proctorResources  Running   openhack/data-load:v1  1.0 core/1.5 gb  Linux     canadacentral
+
+SQL Server: sqlserverdko7020
+SQL Server User Name: sqladmindKo7020
+SQL ServerPassword: oB1lh7Oz8
