6f2a0df21769232756081f0f043b2e52bd401f8f
arturoqu77 <32229456+arturoqu77@users.noreply.github.com>
2021-10-30 10:20:55 -0400

    cleanup

--- tooling/teamdeploy/ContainersSimulatorV2/push-images.sh
+++
@@ -1,6 +0,0 @@
-REPO=$1
-TAG=$2
-
-docker push $REPO/grafana-sim:$TAG
-docker push $REPO/prometheus-sim:$TAG
-docker push $REPO/simulator:$TAG
--- tooling/teamdeploy/ContainersSimulatorV2/tag-images.sh
+++
@@ -1,6 +0,0 @@
-REPO=$1
-TAG=$2
-
-docker tag grafana-sim $REPO/grafana-sim:$TAG
-docker tag prometheus-sim $REPO/prometheus-sim:$TAG
-docker tag simulator $REPO/simulator:$TAG
